9748fd2a95faa8d0b8f38ed2f4385e971daecbd8
arthur <arthur.osipenko@gmail.com>
2018-08-04 17:30:51 +0300

    high-ordered-func

---
+++ high-ordered-func.js
@@ -0,0 +1,12 @@
+const double = n => n * 2;
+const doubleMap = numbers => numbers.map(double);
+console.log(doubleMap([2, 3, 4]));
+
+// const double = n => n.points * 2;
+// const doubleMap = numbers => numbers.map(double);
+
+// console.log(doubleMap([
+//   { name: 'ball', points: 2 },
+//   { name: 'coin', points: 3 },
+//   { name: 'candy', points: 4}
+// ]));
