e0f2e60e6bcff3743f2a2eba664d67207d3ca9e3
arthur <arthur.osipenko@gmail.com>
2018-08-04 19:14:52 +0300

    recursion

---
+++ recursion/groupBy.js
@@ -0,0 +1,30 @@
+const categories = [
+  { name: 'tech', parent: null },
+  { name: 'hot_right_now', parent: 'tech' },
+  { name: 'upcomming_releases', parent: 'tech' },
+  { name: 'gadgets', parent: 'tech' },
+  { name: 'news', parent: null },
+  { name: 'social', parent: 'startups' },
+  { name: 'europe', parent: 'news' },
+  { name: 'asia', parent: 'news' },
+  { name: 'events', parent: 'news' },
+  { name: 'startups', parent: null },
+  { name: 'funding', parent: 'startups' },
+  { name: 'unicorns', parent: 'startups' },
+  { name: 'venture_capital', parent: 'funding' },
+  { name: 'crowdfunding', parent: 'funding' },
+  { name: 'usa', parent: 'news' }
+]
+
+const arrangeCategories = (categories, parent = null) => {
+  let result = {}
+  categories
+    .filter(category => category.parent === parent)
+    .forEach(category => {
+      result[category.name] = arrangeCategories(categories, category.name)
+    })
+
+  return result
+}
+
+console.log(arrangeCategories(categories));
---
+++ recursion/reverse.js
@@ -0,0 +1,14 @@
+function factorial(x) {
+  if (x < 0) return;
+  if (x === 0) return 1;
+  return x * factorial(x - 1);
+}
+
+console.log(factorial(3));
+
+function revStr(str){
+  if (str === '') return '';
+  return revStr(str.substr(1)) + str[0];
+}
+
+console.log(revStr('cat'));
