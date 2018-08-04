24fb672f4649a4b606b917b555b7b137de151194
arthur <arthur.osipenko@gmail.com>
2018-08-04 17:47:06 +0300

    declarative vs imperative

---
+++ declarative-vs-imperative.js
@@ -0,0 +1,13 @@
+const doubleMap = numbers => {
+  const doubled = [];
+  for (let i = 0; i < numbers.length; i++) {
+    doubled.push(numbers[i] * 2);
+  }
+  return doubled;
+};
+
+console.log(doubleMap([2, 3, 4]));
+
+// const doubleMap = numbers => numbers.map(n => n * 2);
+
+// console.log(doubleMap([2, 3, 4]));
